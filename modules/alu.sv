fbc91424f21359a64aa037282b2190ac2c2431bc
marcost2 <marcostolcachir@gmail.com>
2021-11-03 16:00:14 -0300

    Añadir flags para Carry, overflow y negativo

--- modules/alu.sv
+++ modules/alu.sv
@@ -2,19 +2,23 @@ module alu (input logic [63:0] a,
 				input logic [63:0] b,
 				input logic [3:0] ALUControl,
 				output logic [63:0] result,
-				output logic zero);
+				output logic zero, negative, Carry, overflow);
+	logic [64:0] temp_result;
 	always_comb
 		begin
 			casez(ALUControl)
-				4'b0000: result = a & b;
-				4'b0001: result = a | b;
-				4'b0010: result = a + b;
-				4'b0110: result = a - b;
-				4'b0111: result = b;
-				4'b1100: result = ~(a | b);
-				default: result = 64'b0;
+				4'b0000: temp_result = a & b;
+				4'b0001: temp_result = a | b;
+				4'b0010: temp_result = a + b;
+				4'b0110: temp_result = a - b;
+				4'b0111: temp_result = b;
+				4'b1100: temp_result = ~(a | b);
+				default: temp_result = 65'b0;
 			endcase
-			if (result === 64'b0) zero = 1;
-			else zero = 0;
 		end
+	assign result = temp_result[63:0];
+	assign zero = (result === 64'b0) ? 1'b1 : 1'b0;
+	assign negative = (result[63] === 1'b1) ? 1'b1 : 1'b0;
+	assign overflow = 1'b0;
+	assign Carry = temp_result[64];
 endmodule
