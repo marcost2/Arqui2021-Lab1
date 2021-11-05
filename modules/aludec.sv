cf5c6cd329c765cb627222926ae771b6dd68ee00
marcost2 <marcostolcachir@gmail.com>
2021-11-05 15:19:02 -0300

    Add adds and subs to decoder

--- modules/aludec.sv
+++ modules/aludec.sv
@@ -9,6 +9,8 @@ module aludec (input  logic [10:0] funct,
 		else if (aluop == 2'b01) alucontrol = 4'b0111; 	//CBZ
 		else if((aluop == 2'b10)  & (funct == 11'b10001011000)) alucontrol = 4'b0010;	//ADD
 		else if((aluop == 2'b10)  & (funct == 11'b11001011000)) alucontrol = 4'b0110;	//SUB
+		else if((aluop == 2'b10)  & (funct == 11'b10101011000)) alucontrol = 4'b1010;	//ADDS
+		else if((aluop == 2'b10)  & (funct == 11'b11101011000)) alucontrol = 4'b1110;	//SUBS
 		else if((aluop == 2'b10)  & (funct == 11'b10001010000)) alucontrol = 4'b0000;	//AND
 		else if((aluop == 2'b10)  & (funct == 11'b10101010000)) alucontrol = 4'b0001;	//OR    
 		else alucontrol = 4'b0000;
